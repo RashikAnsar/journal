6d837cb3059a6312ec3bfd0fed113ff3c4131b95
RashikAnsar <sk.ansar46@gmail.com>
2019-01-14 13:11:36 +0530

    journal app basic version

---
+++ public/electron.js
@@ -0,0 +1,198 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden'
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open Folder',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            openDir();
+          }
+        },
+        {
+          label: 'Save File',
+          accelerator: 'CmdOrCtrl+S',
+          click() {
+            mainWindow.webContents.send('save-file');
+          }
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [{ role: 'minimize' }, { role: 'close' }]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            require('electron').shell.openExternal('https://electronjs.org');
+          }
+        }
+      ]
+    },
+    {
+      label: 'Developer',
+      submenu: [
+        {
+          label: 'Toggle Developer Tools',
+          accelerator:
+            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click() {
+            mainWindow.webContents.toggleDevTools();
+          }
+        }
+      ]
+    }
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    });
+
+    // Edit menu
+    template[2].submenu.push(
+      { type: 'separator' },
+      {
+        label: 'Speech',
+        submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }]
+      }
+    );
+
+    // Window menu
+    template[4].submenu = [
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' }
+    ];
+  }
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here
+
+function openFile() {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [{ name: 'Markdown', extensions: ['md', 'markdown', 'txt'] }]
+  });
+  if (!files) return;
+
+  const file = files[0];
+  const fileContent = fs.readFileSync(file).toString();
+  // console.log(fileContent);
+  mainWindow.webContents.send('new-file', fileContent);
+}
+
+function openDir() {
+  const directory = dialog.showOpenDialog(mainWindow, {
+    properties: ['openDirectory']
+  });
+  if (!directory) return;
+  const dir = directory[0];
+  mainWindow.webContents.send('new-dir', dir);
+}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,313 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import styled from 'styled-components';
+import dateFns from 'date-fns';
+import brace from 'brace';
+import 'brace/mode/markdown';
 import './App.css';
 
+const settings = window.require('electron-settings');
+const { ipcRenderer } = window.require('electron');
+const fs = window.require('fs');
 class App extends Component {
+  state = {
+    loadedFile: '',
+    filesData: [],
+    activeIndex: 0,
+    newEntry: false,
+    newEntryName: '',
+    directory: settings.get('directory') || null
+  };
+  constructor() {
+    super();
+
+    const directory = settings.get('directory');
+    if (directory) {
+      this.loadAndReadFiles(directory);
+    }
+
+    ipcRenderer.on('save-file', event => {
+      this.saveFile();
+    });
+
+    ipcRenderer.on('new-dir', (event, directory) => {
+      this.setState({
+        directory
+      });
+      settings.set('directory', directory);
+      this.loadAndReadFiles(directory);
+    });
+  }
+
+  loadAndReadFiles = directory => {
+    fs.readdir(directory, (err, files) => {
+      const filteredFiles = files.filter(file => file.includes('.md'));
+      const filesData = filteredFiles.map(file => {
+        const date = file.substr(
+          file.indexOf('_') + 1,
+          file.indexOf('.') - file.indexOf('_') - 1
+        );
+        return {
+          date,
+          path: `${directory}\\${file}`,
+          title: file.substr(0, file.indexOf('_'))
+        };
+      });
+
+      filesData.sort((a, b) => {
+        const aDate = new Date(a.date);
+        const bDate = new Date(b.date);
+        const aSec = aDate.getTime();
+        const bSec = bDate.getTime();
+        return bSec - aSec;
+      });
+      this.setState(
+        {
+          filesData
+        },
+        () => this.loadFile(0)
+      );
+    });
+  };
+
+  changeFile = index => () => {
+    const { activeIndex } = this.state;
+    if (index !== activeIndex) {
+      this.saveFile();
+      this.loadFile(index);
+    }
+  };
+
+  loadFile = index => {
+    const { filesData } = this.state;
+    const content = fs.readFileSync(filesData[index].path).toString();
+
+    this.setState({
+      loadedFile: content,
+      activeIndex: index
+    });
+  };
+
+  saveFile = () => {
+    const { activeIndex, loadedFile, filesData } = this.state;
+    fs.writeFile(filesData[activeIndex].path, loadedFile, err => {
+      if (err) return console.log(err);
+      console.log('File saved');
+    });
+  };
+
+  newFile = e => {
+    e.preventDefault();
+    const { newEntryName, directory, filesData } = this.state;
+    const fileDate = dateFns.format(new Date(), 'MM-DD-YYYY');
+    const filePath = `${directory}\\${newEntryName}_${fileDate}.md`;
+    fs.writeFile(filePath, '', err => {
+      if (err) return console.log(err);
+      filesData.unshift({
+        path: filePath,
+        date: fileDate,
+        title: newEntryName
+      });
+      this.setState({
+        newEntry: false,
+        newEntryName: '',
+        loadedFile: '',
+        filesData
+      });
+    });
+  };
+
   render() {
+    const {
+      activeIndex,
+      directory,
+      filesData,
+      loadedFile,
+      newEntry,
+      newEntryName
+    } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrap>
+        <Header>Journal</Header>
+        {directory ? (
+          <Split>
+            <FilesWindow>
+              <Button onClick={() => this.setState({ newEntry: !newEntry })}>
+                + New File
+              </Button>
+              {newEntry && (
+                <form onSubmit={this.newFile}>
+                  <input
+                    className="fileinput"
+                    autoFocus
+                    value={newEntryName}
+                    onChange={e =>
+                      this.setState({ newEntryName: e.target.value })
+                    }
+                    type="text"
+                  />
+                </form>
+              )}
+              {filesData.map((file, index) => (
+                <FileButton
+                  active={activeIndex === index}
+                  onClick={this.changeFile(index)}
+                >
+                  <p className="title">{file.title}</p>
+                  <p className="date">{formatDate(file.date)}</p>
+                </FileButton>
+              ))}
+            </FilesWindow>
+            <CodeWindow>
+              <AceEditor
+                mode="markdown"
+                onChange={newContent => {
+                  this.setState({ loadedFile: newContent });
+                }}
+                name="markdown_editor"
+                value={loadedFile}
+              />
+            </CodeWindow>
+            <RenderedWindow className="scrollbar" id="scroll-style">
+              <Markdown>{loadedFile}</Markdown>
+            </RenderedWindow>
+          </Split>
+        ) : (
+          <LoadingMessage>
+            <h1>Use Ctrl + O to open directory</h1>
+          </LoadingMessage>
+        )}
+      </AppWrap>
     );
   }
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  margin-top: 23px;
+`;
+
+const Header = styled.header`
+  background-color: #0092ca;
+  color: #eee;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.2);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`;
+
+const LoadingMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #222831;
+  height: 100vh;
+`;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const FilesWindow = styled.div`
+  background: #393e46;
+  border-right: solid 1px #302b3a;
+  position: relative;
+  width: 20%;
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    pointer-events: none;
+    box-shadow: -10px 0 20px rgba(0, 0, 0, 0.3) inset;
+  }
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #222831;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #393e46;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #0092ca;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
+`;
+
+const FileButton = styled.button`
+  padding: 10px;
+  width: 100%;
+  background: #222831;
+  opacity: 0.4;
+  color: white;
+  border: none;
+  text-align: left;
+  border-bottom: solid 1px #302b3a;
+  transition: 0.3s ease all;
+  &:hover {
+    opacity: 1;
+    border-left: solid 4px #0092ca;
+  }
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+    border-left: solid 4px #0092ca;
+  `};
+  .title {
+    font-weight: bold;
+    font-size: 0.9rem;
+    margin: 0 0 5px;
+  }
+  .date {
+    margin: 0;
+  }
+`;
+
+const Button = styled.button`
+  background: transparent;
+  color: white;
+  display: block;
+  border: solid 1px #0092ca;
+  border-radius: 4px;
+  margin: 1rem auto;
+  font-size: 1rem;
+  transition: 0.3s ease all;
+  padding: 5px 10px;
+  &:hover {
+    background: #0092ca;
+    color: #222831;
+  }
+`;
+
+const formatDate = date => dateFns.format(new Date(date), 'MMMM Do YYYY');
